5ca84ba80be28c6780b0dad41d4557aae70492ac
Takahiro HATORI <hatotaka@hatotaka.com>
2016-03-15 11:47:31 +0900

    update

--- main.go
+++ main.go
@@ -10,14 +10,8 @@ var version = 4
 
 func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!<br>\n")
-	fmt.Fprintf(w, "version %d<br>\n", version)
-
-	env, notEmpty := os.LookupEnv("HELLOWORLD_ENV")
-	if notEmpty {
-		fmt.Fprintf(w, "HELLOWORLD_ENV=%d<br>\n", env)
-	}else{
-		fmt.Fprintf(w, "HELLOWORLD_ENV=null<br>\n", env)
-	}
+	fmt.Fprintf(w, "Version=%d<br>\n", version)
+	fmt.Fprintf(w, "HELLOWORLD_ENV=%s<br>\n", os.Getenv("HELLOWORLD_ENV")
 
 }
 
